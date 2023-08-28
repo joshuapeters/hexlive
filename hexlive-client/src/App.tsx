c0681deeba69f4663b4e8f6b5c061c26286163c0
JP <jp@Quiet-Swami.local>
2023-08-27 20:51:08 -0400

    get a beautiful canvas with content setup

--- hexlive-client/src/App.tsx
+++ hexlive-client/src/App.tsx
@@ -1,6 +1,32 @@
 import Konva from "konva";
 import { useState } from "react";
-import { Layer, Rect, Stage, Text } from "react-konva"
+import { Layer, Rect, Stage } from "react-konva"
+import styled from "styled-components";
+
+const ContentContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  position: relative;
+  z-index: 1;
+  padding: 1rem 2rem;
+  background-color: aliceblue;
+  margin-left: 2rem;
+  margin-top: 2rem;
+`;
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  position: relative;
+`;
+
+const StyledStage = styled(Stage)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: transparent;
+`;
 
 const ColoredRect = () => {
   const [color, setColor] = useState('green');
@@ -11,25 +37,32 @@ const ColoredRect = () => {
 
   return (
     <Rect
-      x={20}
-      y={20}
+      x={100}
+      y={100}
       width={50}
       height={50}
       fill={color}
       shadowBlur={5}
       onClick={handleClick}
+      draggable
     />
   );
 };
 
 function App() {
   return (
-    <Stage width={window.innerWidth} height={window.innerHeight}>
-      <Layer>
-        <Text text="Try click on rect" />
-        <ColoredRect />
-      </Layer>
-    </Stage>
+    <>
+      <CanvasContainer>
+        <StyledStage width={window.outerWidth} height={window.outerWidth}>
+          <Layer draggable>
+            <ColoredRect />
+          </Layer>
+        </StyledStage>
+      </CanvasContainer>
+      <ContentContainer>
+        <p style={{ color: "black" }}>hello</p>
+      </ContentContainer>
+    </>
   )
 }
 
